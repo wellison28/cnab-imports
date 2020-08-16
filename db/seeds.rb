0c0bfc8edaf3c06ef4e1d3660d5c17ee14617eed
Wellison Souza <wellison28@gmail.com>
2020-08-16 11:45:30 -0300

    create seed for load operations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+transaction_types = [
+  { kind: 1, description: 'Débito', operation: 0 },
+  { kind: 2, description: 'Boleto', operation: 1 },
+  { kind: 3, description: 'Financiamento', operation: 1 },
+  { kind: 4, description: 'Crédito', operation: 0 },
+  { kind: 5, description: 'Recebimento Empréstimo', operation: 0 },
+  { kind: 6, description: 'Vendas', operation: 0 },
+  { kind: 7, description: 'Recebimento TED', operation: 0 },
+  { kind: 8, description: 'Recebimento DOC', operation: 0 },
+  { kind: 9, description: 'Aluguel', operation: 1 }
+]
+
+TransactionType.insert_all!(transaction_types)
